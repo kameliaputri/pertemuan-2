c9bd8888521bd629258f38e78334eba12484c303
kameliaputri <53284562+kameliaputri@users.noreply.github.com>
2019-10-09 11:41:25 +0700

    Add files via upload

---
+++ latihan 2.php
@@ -0,0 +1,35 @@
+<?php
+
+//looping /pengulangan
+
+// type 1 = FOR
+//for ($i=0; $i <= 10; $i++ ) {
+	//echo $i . "kamelia putri <br>"; 
+//}
+
+//type 2 =WHILE
+//$i =1;
+//while ($i <= 10){
+//	echo $i . "kamel <br>";
+	//$i++;
+//}
+
+//type 3 = FOREACH
+
+//conditional / pengkondisian
+//$angka = 10;
+//if ($angka >10) {
+	//echo "lebih besar dari 10";
+//}else if ($angka ==10 ) {
+	//echo "sama dengan 10";
+//}else {
+	//echo "kurang dari 10";
+//}
+
+//tugasnya : buatkan sebuah array 1 dimensi dan lebih dari 1 dimensi
+//Tampilkan dalam bentuk pengulangan FOREACH
+
+$cars = array(" mie", "es krim", "kue");
+echo "aku suka" . $cars[0] . ", " . $cars[1] . " dan " . $cars[2] . ".";
+
+?>
